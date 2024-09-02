3ddee0f79f1971f5b8c4546be65f8c3c4e111e87
Leonardo Melo Santos <leonardomelosantos@gmail.com>
2024-09-01 23:27:31 -0300

    Commited stable version of Ex4 of Cases02

--- src/Cases02/Ex4/Ex4.cs
+++ src/Cases02/Ex4/Ex4.cs
@@ -13,22 +13,178 @@ D. Compare o tempo de execução das três implementações.
 */
 
 using System;
+using System.Collections.Concurrent;
+using System.Diagnostics;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Lab02.Ex4;
 
-class Program {
+class Program
+{
     static void Main(string[] args)
+    {
+        (int N, int[] sequence) = GetInputData();
+
+        // Continue a Implementação
+        var timeForTasks = new PrimosTasks().Execute(sequence);
+        var timeForParallelFor = new PrimosParallelFor().Execute(sequence);
+        var timeForParallelForEach = new PrimosParallelForEach().Execute(sequence);
+
+        Console.WriteLine($"Tasks: {timeForTasks}ms; " +
+            $"ParallelFor: {timeForParallelFor}ms, " +
+            $"ParallelForEach: {timeForParallelForEach}ms");
+    }
+
+    private static (int N, int[] sequence) GetInputData()
     {
         int N = int.Parse(Console.ReadLine());
         string[] input = Console.ReadLine().Split(' ');
         int[] sequence = new int[N];
 
-        for(int i = 0; i < N; i++) {
+        for (int i = 0; i < N; i++)
+        {
             sequence[i] = int.Parse(input[i]);
         }
+        return (N, sequence);
+    }
+}
 
-        // Continue a Implementação
-        // ...
+class PrimosTasks : PrimosBase
+{
+    /// <summary>
+    /// Using Tasks
+    /// </summary>
+    protected override void PrintPrimesNumbers(int[] sequence)
+    {
+        IList<Task> tasks = new List<Task>();
+        foreach (int numberK in sequence)
+        {
+            Task taskForK = Task.Run(() =>
+            {
+                List<int> primes = GetPrimesFrom(numberK);
+                PrintPrimesResult(numberK, primes);
+            });
+            tasks.Add(taskForK);
+        }
+        Task.WaitAll(tasks.ToArray());
+    }
+
+    protected override List<int> GetPrimesFrom(int k)
+    {
+        List<int> primes = new();
+        for (int number = 1; number <= k; number++)
+        {
+            if (IsPrime(number))
+            {
+                primes.Add(number);
+            }
+        };
+        return primes;
+    }
+}
+
+
+class PrimosParallelFor : PrimosBase
+{
+    /// <summary>
+    /// Using Parallel.For
+    /// </summary>
+    protected override void PrintPrimesNumbers(int[] sequence)
+    {
+        Parallel.For(0, sequence.Length, (index) =>
+        {
+            int k = sequence[index];
+            List<int> primes = GetPrimesFrom(k);
+            PrintPrimesResult(k, primes);
+        });
+    }
+
+    /// <summary>
+    /// Using Parallel.For
+    /// </summary>
+    protected override List<int> GetPrimesFrom(int k)
+    {
+        ConcurrentBag<int> primes = new ConcurrentBag<int>();
+        Parallel.For(1, k + 1, (number) =>
+        {
+            if (IsPrime(number))
+            {
+                primes.Add(number);
+            }
+        });
+        return primes.ToList();
+    }
+}
+
+
+class PrimosParallelForEach : PrimosBase
+{
+    /// <summary>
+    /// Using Parallel.ForEach
+    /// </summary>
+    protected override void PrintPrimesNumbers(int[] sequence)
+    {
+        Parallel.ForEach<int>(sequence, (k) =>
+        {
+            List<int> primes = GetPrimesFrom(k);
+            PrintPrimesResult(k, primes);
+        });
+    }
+
+    /// <summary>
+    /// Using Parallel.ForEach
+    /// </summary>
+    protected override List<int> GetPrimesFrom(int k)
+    {
+        ConcurrentBag<int> primes = new ConcurrentBag<int>();
+        Parallel.ForEach<int>(Enumerable.Range(1, k), (number) => 
+        {
+            if (IsPrime(number))
+            {
+                primes.Add(number);
+            }
+        });
+        return primes.ToList();
+    }
+}
+
+abstract class PrimosBase
+{
+    abstract protected void PrintPrimesNumbers(int[] sequence);
+    abstract protected List<int> GetPrimesFrom(int k);
+
+    public double Execute(int[] sequence)
+    {
+        Stopwatch timer = new();
+        timer.Start();
+
+        PrintPrimesNumbers(sequence); // Abstract
+
+        timer.Stop();
+        return timer.ElapsedMilliseconds;
+    }
+
+    protected static bool IsPrime(int number)
+    {
+        if (number < 2)
+        {
+            return false;
+        }
+        for (int i = 2; i <= Math.Sqrt(number); i++)
+        {
+            if (number % i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    protected static void PrintPrimesResult(int k, List<int> primesToK)
+    {
+        var intsToStrs = primesToK.Select(number => number.ToString()).ToList();
+        var strsJoined = string.Join(", ", intsToStrs);
+        Console.WriteLine($"Primes (1..{k}): {strsJoined}");
     }
 }
