591fd6b11be58ed46f9055514be48a09e24bf6cb
Leonardo Melo Santos <leonardomelosantos@gmail.com>
2024-09-23 17:15:47 -0300

    Finished Ex4 of Cases 03

--- src/Cases03/Ex4/Ex4.cs
+++ src/Cases03/Ex4/Ex4.cs
@@ -4,7 +4,7 @@ Descrição: A FastDelivery está desenvolvendo um sistema automatizado para ger
 suas entregas expressas. Nesse sistema, os Vendedores são responsáveis por registrar
 solicitações de entrega, enquanto os Entregadores coletam essas solicitações e as
 entregam aos clientes. Para garantir um fluxo eficiente, o sistema utiliza uma fila
-compartilhada e segura para comunicação entre vendedores e entregadores. Considere que
+compartilhada e segura para comunicação entre vendedores e entregadoresTasks. Considere que
 todos os Vendedores vendem os mesmos produtos e todos os Entregadores podem
 entregar qualquer produto.
 
@@ -27,9 +27,7 @@ Vendedor 2: Pedido #002 criado.
 Entregador 2: Pedido #002 entregue.
 */
 
-using System;
-using System.Threading;
-using System.Threading.Tasks;
+using System.Collections.Concurrent;
 
 namespace Lab03.Ex4;
 
@@ -37,13 +35,89 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Leitura do número total de _pedidos (N), vendedores (V) e entregadores (E)
+        // Leitura do número total de pedidos (N), vendedores (V) e entregadoresTasks (E)
         string[] entrada = Console.ReadLine().Split();
         int N = int.Parse(entrada[0]);
         int V = int.Parse(entrada[1]);
         int E = int.Parse(entrada[2]);
 
         // Continue a implementação
+        FastDelivery fastDelivery = new(N);
+        fastDelivery.ProcessarPedidos(N, V, E);
+    }
+}
 
+public class FastDelivery
+{
+    private const int DELAY_PROCESSAMENTO = 300;
+    private const int DELAY_PROCESSAMENTO_FILA = 600;
+
+    private ConcurrentQueue<int> _totalDeSolicitacoes;
+    private BlockingCollection<int> _pedidosLiberadosParaEntrega;
+
+    private List<Task> entregadoresTasks = new();
+    private List<Task> vendedoresTasks = new();
+
+
+    public FastDelivery(int totalPedidos)
+    {
+        _totalDeSolicitacoes = new();
+        _pedidosLiberadosParaEntrega = new(totalPedidos);
+    }
+
+    public void ProcessarPedidos(int N, int V, int E)
+    {
+        for (int indexPedido = 1; indexPedido <= N; indexPedido++)
+        {
+            _totalDeSolicitacoes.Enqueue(indexPedido); // Registrar solicitação/pedido numa fila
+        }
+
+        for (int indexVendedor = 1; indexVendedor <= V; indexVendedor++)
+        {
+            int identificacaoVendedor = indexVendedor;
+            Task vendaTask = Task.Run(async () => { await EfetivarVendaDeAlgumPedidoAsync(identificacaoVendedor); });
+
+            vendedoresTasks.Add(vendaTask);
+            entregadoresTasks.Add(vendaTask);
+        }
+
+        for (int indexEntregador = 1; indexEntregador <= E; indexEntregador++)
+        {
+            int identificacaoEntregador = indexEntregador;
+            Task entregadorTask = Task.Run(() => { EntregarPedidoVendido(identificacaoEntregador); });
+            entregadoresTasks.Add(entregadorTask);
+        }
+
+        Task.WhenAll(vendedoresTasks)
+            .ContinueWith((act) => { FinalizarProcesso(); });
+
+        Task.WaitAll(entregadoresTasks.ToArray());
+    }
+
+    internal void FinalizarProcesso()
+    {
+        _pedidosLiberadosParaEntrega.CompleteAdding();
+    }
+
+    internal async Task EfetivarVendaDeAlgumPedidoAsync(int vendedor)
+    {
+        Random numeroAleatorio = new Random();
+        await Task.Delay(numeroAleatorio.Next(1, DELAY_PROCESSAMENTO));
+
+        while (_totalDeSolicitacoes.TryDequeue(out int pedido))
+        {
+            Console.WriteLine($"Vendedor {vendedor}: Pedido #{pedido:000} criado.");
+            _pedidosLiberadosParaEntrega.Add(pedido);
+
+            await Task.Delay(numeroAleatorio.Next(1, DELAY_PROCESSAMENTO_FILA));
+        }
+    }
+
+    internal void EntregarPedidoVendido(int entregador)
+    {
+        foreach (int pedido in _pedidosLiberadosParaEntrega.GetConsumingEnumerable())
+        {
+            Console.WriteLine($"Entregador {entregador}: Pedido #{pedido:000} entregue.");
+        }
     }
 }
